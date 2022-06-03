bd1e31e75ba028ef01419b20daf4db1a84c98c9c
EdmondTann <335737557@gapps.yrdsb.ca>
2022-06-03 09:59:20 -0400

    bear

--- Bear.java
+++ Bear.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Dog extends Actor
+public class Bear extends Actor
 {
     /**
      * Act - do whatever the Dog wants to do. This method is called whenever
@@ -14,6 +14,12 @@ public class Dog extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("a")){
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            move(2);
+        }
+        
     }    
 }
